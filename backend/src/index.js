1e830d50323fdfd4005b4253a35cddf8c981e7ff
mahin273 <md.mahin.bd18@gmail.com>
2025-11-03 21:56:08 +0600

    added react-query for api fetching

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import {PORT}  from './config/serverConfig.js';
 import cors from 'cors';
+import apiRouter from "./routes/index.js"
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/api',apiRouter)
 
 app.listen(PORT,()=>{
   console.log(`Server is running on port ${PORT}`); 
